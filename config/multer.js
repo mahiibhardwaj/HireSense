4f85ee7726588c5c2eecd27de730554762e0d68a
mahiibhardwaj <mahi2509mabhardwaj@gmail.com>
2026-07-24 23:28:05 +0530

    Fix multer upload path for Render

--- config/multer.js
+++ config/multer.js
@@ -1,9 +1,17 @@
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
+
+const uploadDir = path.join(process.cwd(), "uploads");
+
+// Create uploads folder if it doesn't exist
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/");
+    cb(null, uploadDir);
   },
 
   filename: (req, file, cb) => {
@@ -21,6 +29,9 @@ const fileFilter = (req, file, cb) => {
   }
 };
 
-const upload = multer({ storage, fileFilter });
+const upload = multer({
+  storage,
+  fileFilter
+});
 
 module.exports = upload;
